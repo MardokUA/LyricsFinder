1ab62f39c7fab0690db5b88fb00a096b5543aa2c
Roman Laktionov <r.a.laktionov@gmail.com>
2018-05-23 16:24:59 +0300

    - pin drawables added;

---
+++ app/src/main/java/com/gmail/laktionov/sample/rx/lyricsfinder/version2/pin/PinActivity.kt
@@ -0,0 +1,16 @@
+package com.gmail.laktionov.sample.rx.lyricsfinder.version2.pin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by: Roman Laktionov
+ * Date: 22.05.18
+ */
+class PinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
