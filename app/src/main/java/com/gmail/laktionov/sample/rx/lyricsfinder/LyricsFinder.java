91caa400cf0eccc223765eb798ef29cda2f1e5f5
Roman Laktionov <r.a.laktionov@gmail.com>
2018-05-24 09:13:18 +0300

    - pin activity added;

--- app/src/main/java/com/gmail/laktionov/sample/rx/lyricsfinder/LyricsFinder.java
+++ app/src/main/java/com/gmail/laktionov/sample/rx/lyricsfinder/LyricsFinder.java
@@ -1,19 +1,10 @@
 package com.gmail.laktionov.sample.rx.lyricsfinder;
 
 import android.app.Application;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
 
-import com.gmail.laktionov.sample.rx.lyricsfinder.version1.core.factory.PresenterFactory;
 import com.gmail.laktionov.sample.rx.lyricsfinder.version2.DIManager;
 import com.gmail.laktionov.sample.rx.lyricsfinder.version2.core.LyricRepository;
 import com.gmail.laktionov.sample.rx.lyricsfinder.version2.core.ViewModelFactory;
-import com.gmail.laktionov.sample.rx.lyricsfinder.version2.core.datasource.LocalSource;
-import com.gmail.laktionov.sample.rx.lyricsfinder.version2.core.datasource.RemoteSource;
-import com.gmail.laktionov.sample.rx.lyricsfinder.version2.core.datasource.Repository;
-import com.gmail.laktionov.sample.rx.lyricsfinder.version2.core.datasource.local.DataBase;
-import com.gmail.laktionov.sample.rx.lyricsfinder.version2.core.datasource.local.LocalDataSource;
-import com.gmail.laktionov.sample.rx.lyricsfinder.version2.core.datasource.remote.RemoteDataSource;
 
 public class LyricsFinder extends Application {
 
