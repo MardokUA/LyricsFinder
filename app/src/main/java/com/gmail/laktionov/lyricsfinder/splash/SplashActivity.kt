b244f5bec27a7f7517263993d383beb722e67624
Roman Laktionov <r.a.laktionov@gmail.com>
2019-04-24 16:56:05 +0300

    - start animation added;

--- app/src/main/java/com/gmail/laktionov/lyricsfinder/splash/SplashActivity.kt
+++ app/src/main/java/com/gmail/laktionov/lyricsfinder/splash/SplashActivity.kt
@@ -1,10 +1,15 @@
 package com.gmail.laktionov.lyricsfinder.splash
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.gmail.laktionov.lyricsfinder.R
 import com.gmail.laktionov.lyricsfinder.search.SearchActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
@@ -15,13 +20,20 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun delayStart() {
-        Thread {
-            Thread.sleep(2_000L)
-            runOnUiThread {
-                startActivity(Intent(this@SplashActivity, SearchActivity::class.java).also {
-                    it.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP and Intent.FLAG_ACTIVITY_NEW_TASK)
-                })
+
+        val animator = ObjectAnimator
+                .ofFloat(app_label, View.ALPHA, 0f, 1f)
+                .setDuration(1_500L)
+
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                val searchIntent = Intent(this@SplashActivity, SearchActivity::class.java).also {
+                    it.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                }
+                startActivity(searchIntent)
+                finish()
             }
-        }.run()
+        })
+        animator.start()
     }
 }
